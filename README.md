78cad678dbe0f05426f54dc917cacaa28fe9160c
sjk <s.james0123456789@gmail.com>
2021-11-11 23:00:40 +1100

    Init

---
+++ README.md
@@ -0,0 +1,169 @@
+# Algorand Vanity Address Generator
+
+A simple vanity address generator for the Algorand blockchain.
+
+Computes thousands of addresses every second and searches them for a desired set of starting characters.
+On my 6 year old MacBook Pro I was able to compute 60-70k addresses a second.
+
+For example:
+
+```
+$ python generate.py TEST output.json -n 5
+Using 8 process(es) to search for 5 Algorand address(es) starting with TEST
+Searched addresses:             0 (~0/sec)
+Found! TESTRPP2VOXSYHT5CZNH6DNFVMXEBR7453BFOKOPRLYTAIDNESFQZZMLRQ
+Searched addresses:       120,000 (~60,000/sec)
+Found! TESTUUJESXXWADGAOGC5BKNHQUYUOM7GSGWKQWGRL7UMAJ2ZQ73JVA3H3U
+Searched addresses:       462,000 (~64,500/sec)
+Found! TESTMRP6P5WVHLI6Y6QE57FBGY3KNF36GMBWCFSNE5P7RHNYRQKYBFU76E
+Searched addresses:     2,866,000 (~68,500/sec)
+Found! TESTYQQE6PNYYZCARF5E2MM4IVRGN7B7YJQTUITCPOAHDKBB7P74HRQC5U
+Searched addresses:     3,973,000 (~66,500/sec)
+Found! TESTISIAY3G2HQOI5MSGUOMEEDUL45E5VF7DABGKAY3XO3E3VGEEDHQ4XY
+```
+
+With the output being a JSON file `output.json`
+
+```json
+[
+  {
+    "address": "TESTRPP2VOXSYHT5CZNH6DNFVMXEBR7453BFOKOPRLYTAIDNESFQZZMLRQ",
+    "mnemonic": "word word word..."
+  },
+  {
+    "address": "TESTUUJESXXWADGAOGC5BKNHQUYUOM7GSGWKQWGRL7UMAJ2ZQ73JVA3H3U",
+    "mnemonic": "word word word..."
+  },
+  {
+    "address": "TESTMRP6P5WVHLI6Y6QE57FBGY3KNF36GMBWCFSNE5P7RHNYRQKYBFU76E",
+    "mnemonic": "word word word..."
+  },
+  {
+    "address": "TESTYQQE6PNYYZCARF5E2MM4IVRGN7B7YJQTUITCPOAHDKBB7P74HRQC5U",
+    "mnemonic": "word word word..."
+  },
+  {
+    "address": "TESTISIAY3G2HQOI5MSGUOMEEDUL45E5VF7DABGKAY3XO3E3VGEEDHQ4XY",
+    "mnemonic": "word word word..."
+  }
+]
+```
+
+## How to get started
+
+> Note: I've only tested this on a single Intel MacBook Pro.
+> If anyone wants to tell me how it works on other machines
+> I'd really appreciate it.
+
+Prerequisites:
+
+* Git
+* Python 3
+* Some terminal
+
+Open your terminal and clone this repository
+
+```bash
+TODO
+```
+
+Enter it, create the required local environment and install the required libraries:
+
+```bash
+cd TODO
+python3 -m venv env
+source env/bin/activate
+pip install -r requirements.txt
+```
+
+Now you're good to go.
+
+If you ever want to leave the environment you can just call
+
+```bash
+disconnect
+```
+
+And to join the environment again (or if you open a new terminal), all you need this time is
+
+```bash
+source env/bin/activate
+```
+
+## Running the generator
+
+To run the generator use the following command while in the correct environment
+
+```bash
+python generate.py [START] [output-file]
+```
+
+Replace [START] with the characters you want at the beginning of your address.
+Replace [output-file] with the location you want to save the addresses and corresponding mnemonics (the 25 words to access your account).
+
+There are a few optional parameters:
+
+* `-n` or `--number` (e.g. `-n 5`):
+  Stop after we've found this many addresses.
+  If not provided, will continue until the user manually interrupts using `ctrl+c`.
+* `-c` or `--cpu` (e.g. `-c 4` or `-c -2`):
+  Use the given number of cpu cores to process.
+  The more CPU cores the faster it will compute.
+  If none is provided, will use all cores.
+  If a negative number will provided, will use all but that number of cores
+* `-h` or `--help`:
+  Print out the instructions.
+
+As it runs, it will display the total number of addresses checked and a rough estimate of how many addresses it's processing per second.
+
+It will print the address as the 
+
+It writes to the output file as it goes (occasionally producing temporary files to reduce risk of file corruption).
+
+## How long does it take to find a given address?
+
+It depends on the number of CPU cores you use and their speed.
+The longer the START you're looking for is, the longer it takes to find.
+
+The following is a rough calculation of the average number of addresses that need to be searched depending on the length of the address.
+
+| Length | Addresses to search | Rough time on my laptop |
+| ------ | ------------------- | ----------------------- |
+|   1    |   16                |      < 1 seconds               |
+|   2    |   512               |      < 1 seconds               |
+|   3    |   16,384            |      < 1 seconds               |
+|   4    |   524,288           |      ~ 9 seconds               |
+|   5    |   16,777,216           |      ~ 4.5 minutes               |
+|   6    |   536,870,912       |      ~ 2.5 hours |
+|   7    |   17 billion        |      ~ 80 hours |
+|   8    |   549 billion       |      ~ 106 days |
+|  54    |   948 quinvigintillion | ~ 38 octodecillion lifetimes of the universe |
+
+## How trustworthy is this
+
+Good question.
+This code deals with private keys for algorand wallets.
+
+For the sake of clarity surrounding something that is quite security critical, I have kept the code very simple, have avoided all but one external dependency (the official Algorand SDK) and gratuitously over-commented.
+Every chunk of code has corresponding comments describing what it does with the idea being that even someone without much skill in python, or programming in general, can verify my intention.
+
+What I have written is not malicious.
+However I cannot guarantee the libraries I use,
+the python runtime you use or anything on your system.
+This is licensed under the MIT license as such:
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+## I still don't trust you
+
+Good.
+
+If you want to keep the address, but not use the private key the generator made for you, you can re-key the address.
+
+This can easily be performed via the "Rekey Account" option in the official Algorand wallet, though it only works if you have a ledger device you want to Rekey to.
+
+If you have a little bit of developer know-how, you can also re-key using `goal` as described in the [Algorand developer docs](https://developer.algorand.org/docs/get-details/accounts/rekey/).
+
+## Contact details
+
+Twitter: [SJK](https://twitter.com/sjk0_9)
---
+++ generate.py
@@ -0,0 +1,202 @@
+# Each of these imports, besides the AlgoSDK are part of the Python Standard Library
+import argparse                         # Handles the command line inputs
+from multiprocessing import \
+    Process, Queue, Value, cpu_count    # For running on multiple CPU cores
+from queue import Empty                 # For helping read from the queue
+import itertools                        # To help us count the number of addresses we're generating
+import os                               # For managing the files we are writing to
+import json                             # For compiling the JSON we're writing
+import re                               # To check that the requested value is possible
+from algosdk import account, mnemonic   # For generating the accounts
+
+def main(**kwargs):
+    """
+    The core process of the generator.
+    Spins up each subprocess (which does the actual computational work).
+    And retrieves and saves valid addresses while updating the commandline.
+    """
+    start = kwargs['start']
+    output = kwargs['output']
+    number = kwargs['number']
+    cpu = kwargs['cpu']
+
+    # Not all letters are valid. An Algorand Address is made up of upper case
+    # letters A-Z and the numbers 2-7. 0 and 1 are ommited because they look too
+    # similar to O and I. 8 and 9 are uneccessary because it's a 32 bit encoding
+    # so we only need 32 possible characters.
+    # If we've got a character that doesn't match, lets fail before we start
+    # looking, otherwise we'll be here searching forever.
+    if re.match('^[A-Z2-7]+$', start) is None:
+        raise ValueError('Invalid start. Can only contain upper case letters A-Z and numbers 2-7.')
+
+    # Each CPU core will communicate back to the main process via this queue
+    queue = Queue()
+    # The counter will be shared by each subprocess so we can tell how fast
+    # We're generating accounts
+    counter = Value('i', 0)
+
+    # Parse number of CPU cores to use based on user input or default values
+    if cpu < 0:
+        cpu = max(get_max_cpus() + cpu, 1)
+    if cpu == 0:
+        raise ValueError('Cannot have CPU set to 0')
+
+    # Output to let the user know what we're doing
+    if number:
+        print('Using {} process(es) to search for {} Algorand address(es) starting with {}'.format(cpu, number, start))
+    else:
+        print('Using {} process(es) to search for Algorand addresses starting with {}'.format(cpu, start))
+
+    # For each core we can use, start a new python Process, which will run the
+    # code to generate and check addresses in the subprocess method below.
+    processes = []
+    for _ in range(cpu):
+        p = Process(target=subprocess, args=(start, queue, counter))
+        p.start()
+        processes.append(p)
+
+    number_found = 0
+    previous_counted = 0
+    num_counted = 0
+    # Now the processes are underway, keep checking if we've found anything in
+    # the queue
+    try:
+        while True:
+            # Compute and print out the number of addresses we've found and
+            # the rate we're progressing
+            previous_counted = num_counted
+            num_counted = counter.value
+            rate = (num_counted-previous_counted)//2
+            print('\rSearched addresses: {:13,} (~{:,}/sec)'.format(num_counted, rate), end="")
+            try:
+                # Check the queue for 2 seconds at a time, if we don't find anything
+                # start the loop again so we can refresh our counter display
+                item = queue.get(timeout=2)
+            except Empty:
+                continue
+
+            # If we've made it here, it means we've found a matching address!
+            # print the address and add the full details to the JSON file
+            print()
+            print('Found!', item['address'])
+            write_to_json(item, output)
+
+            number_found += 1
+            # If we have a limit on the number we want to return, and we've reached
+            # that limit, break out of this loop
+            if number_found == number:
+                break
+    except KeyboardInterrupt:
+        # In the case that we try to stop early by sending an interrupt, we should
+        # still tidy up, so catch the exception and continue to the cleanup
+        pass
+
+    # Stop and cleanup all of the previous processes
+    for p in processes:
+        p.terminate()
+        p.join()
+
+def subprocess(start, queue, counter):
+    """
+    Each instance of subprocess will continuously generate random accounts
+    and check if they meet our criteria.
+    """
+    for i in itertools.count():
+        try:
+            # Generates a new random private key and the corresponding address
+            private_key, address = account.generate_account()
+            # If the start of the random address matches the phrase we're searching
+            # for we should return it.
+            if address.startswith(start):
+                # Produce the mnemonic (key phrase) and send it back to the main
+                # process along with the address
+                queue.put({
+                    'address': address,
+                    'mnemonic': mnemonic.from_private_key(private_key),
+                })
+
+            # Increment the counter every 1000 cycles
+            # (Any more regularly would cause needless delay)
+            if (i % 1000 == 0):
+                counter.value += 1000
+        except KeyboardInterrupt:
+            # Ignore interrupts when closing, because we are handling them and
+            # the requisite cleanup in the main process
+            pass
+
+def write_to_json(item, output):
+    """
+    Append a given item to an array in a JSON file.
+    If need be, creates the file.
+    Has some fancy stuff in there to reduce risk of file corruption in event of
+    a crash or interrupt.
+    """
+    try:
+        # If the file doesn't exist, just fill it with the first item
+        with open(output, 'x') as f:
+            json.dump([item], f, indent=2)
+    except FileExistsError:
+        # If the file does exist retrieve the list of everything we've found
+        # so far
+        with open(output, 'r') as f:
+            all_items = json.load(f)
+        # Add the new item to that list
+        all_items.append(item)
+
+        # Write the new list to a temporarty file so we don't corrupt the
+        # original in the event of a crash or interrupt
+        temp_output = 'temp-{}'.format(output)
+        with open(temp_output, 'w') as f:
+            json.dump(all_items, f, indent=2)
+
+        # Replace the original file with its updated version
+        os.replace(temp_output, output)
+
+def get_max_cpus():
+    """
+    Returns the maximum number of CPU cores available for us to use
+    """
+    try:
+        return cpu_count()
+    except NotImplementedError:
+        # If we can't figure out how many CPU cores we have... well there's got
+        # to be at least 1 right?
+        return 1
+
+# Run the following code only when we directly call this script from commandline
+if __name__ == "__main__":
+    # The following is what parses the commandline input to determine how to run
+    # the remainder of the script
+    parser = argparse.ArgumentParser(
+        description="""
+        Produces valid Algorand Addresses which begin with a set series of characters.
+        Prints the addresses and writes the addresses and mnemonics in the JSON file format."""
+        )
+    parser.add_argument(
+        'start',
+        type=str,
+        help="The characters you want at the start of your address."
+        )
+    parser.add_argument(
+        'output',
+        help="The file in which to write the mnemonics. If the file already exists, will attempt to read and append to it."
+        )
+    parser.add_argument(
+        '--number',
+        '-n',
+        type=int,
+        help="The number of addresses to produce. If not provided, will continue until manually stopped by the user."
+        )
+    parser.add_argument(
+        '--cpu',
+        '-c',
+        default=get_max_cpus(),
+        type=int,
+        help="The number of CPU cores to use. Default is maximum available. Use negative numbers to define number of CPU cores not to use."
+        )
+
+    args = parser.parse_args()
+    try:
+        main(**vars(args))
+    except Exception as e:
+        print(e)
